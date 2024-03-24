c743ee7ed3097666da170631327e52584daebc1b
khanhduy14 <101503338+khanhduy-14@users.noreply.github.com>
2024-03-24 22:10:49 +0700

    Time: 56 ms (90.16%), Space: 51.9 MB (84.48%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+    const map: Record<number, number> = {}
+    
+    for(let i=0; i< nums.length; i++) {
+        if(map[target - nums[i]] !== undefined) {
+            return [map[target - nums[i]], i];
+        }
+        else {
+            map[nums[i]] = i;
+        }
+    }
+};
