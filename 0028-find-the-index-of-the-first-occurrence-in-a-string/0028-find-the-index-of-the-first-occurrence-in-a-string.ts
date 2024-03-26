24a4370405918d415111fcb2578cf45bc8d098db
khanhduy14 <101503338+khanhduy-14@users.noreply.github.com>
2024-03-26 23:15:05 +0700

    Time: 63 ms (26.41%), Space: 50.4 MB (70.12%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,4 +1,6 @@
 function strStr(haystack: string, needle: string): number {
+  if (needle === '' || needle === haystack) return 0;
+  if (haystack.length < needle.length) return -1;
     for(let i = 0; i< haystack.length; i++) {
         for(let j=0; j< needle.length;j++) {
             if(haystack[i+j] !== needle[j]) {
