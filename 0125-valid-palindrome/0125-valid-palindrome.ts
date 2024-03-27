6a80366f49c5dbe2b588651eb35cfac5c2eff8bc
khanhduy14 <101503338+khanhduy-14@users.noreply.github.com>
2024-03-27 21:53:44 +0700

    Time: 72 ms (39.39%), Space: 52.1 MB (95.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,12 +1,43 @@
 function isPalindrome(s: string): boolean {
-    const str: string = s.toLowerCase().replace(/[^0-9a-zA-Z]/g, '')
-    let l: number = 0, r: number = str.length - 1
+    let l: number = 0, r: number = s.length - 1
     while ( l <= r) {
-        if(str[l]!==str[r]){
-            return false
+        const s1 = s.charCodeAt(l);
+        const s2 = s.charCodeAt(r);
+        
+        if(!isLetter(s1)){
+            l++
+            continue;
+        }
+        
+        if(!isLetter(s2)){
+            r--
+            continue;
+        }
+        if(toLowerCase(s1) !== toLowerCase(s2))
+        {
+            return false;
         }
         l++;
         r--;
     }
     return true
 };
+
+function isLetter(code): boolean {
+    const isLowerCaseLetter = code >= 97 && code <= 122;
+    const isUppercaseLetter = code >= 65 && code <= 90;
+    const isNumberLetter = code >= 48 && code <= 57;
+    if (isLowerCaseLetter || isUppercaseLetter || isNumberLetter) {
+        return true;
+    }
+    return false;
+}
+
+function toLowerCase(code): number  {
+    if (code >= 65 && code <= 90) {
+        return code + 32;
+    }
+    else {
+        return code;
+    }
+}
