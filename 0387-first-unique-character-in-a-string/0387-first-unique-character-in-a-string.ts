d8c75093fa3e9012a68378e21e3e90130357ddf9
khanhduy14 <101503338+khanhduy-14@users.noreply.github.com>
2024-03-28 23:44:25 +0700

    Time: 106 ms (19.17%), Space: 54.6 MB (47.31%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.ts
@@ -0,0 +1,24 @@
+function firstUniqChar(s: string): number {
+    const map: Record<string, number> = {};
+    
+    for (let i = 0; i < s.length; i++) {
+          if (map[s[i]]){
+             map[s[i]]+=1;
+         }
+        else{
+            map[s[i]]=1
+        }
+        
+        
+    }
+      for (let i = 0; i < s.length; i++) {
+        if(map[s[i]]===1) {
+            return i;
+        }
+    }
+    
+    
+    
+  
+    return -1
+};
