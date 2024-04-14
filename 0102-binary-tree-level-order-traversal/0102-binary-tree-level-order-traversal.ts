3feccffa25d88a56249c29a2d7a465ce92a2d734
khanhduy14 <101503338+khanhduy-14@users.noreply.github.com>
2024-04-14 17:27:28 +0700

    Time: 65 ms (98.48%), Space: 55.9 MB (87.85%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.ts
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.ts
@@ -12,58 +12,19 @@
  * }
  */
 
-class QueueNode {
-    
-    dataMap: Record<number, any>;
-    frontIndex: number;
-    backIndex: number;
-    
-    constructor() {
-        this.dataMap = {}
-        this.frontIndex = 0;
-        this.backIndex = 0;
-    }
-
-    enqueue(val: any) {
-        this.dataMap[this.backIndex] = val;
-        this.backIndex++;
-    }
-    dequeue() {
-        const item = this.dataMap[this.frontIndex];
-        delete this.dataMap[this.frontIndex];
-        this.frontIndex++;
-        return item;
-    }
-    length() {
-        return this.backIndex - this.frontIndex;
-    }
-}
 
 function levelOrder(root: TreeNode | null): number[][] {
-    const queue = new QueueNode();
-    const result: number[][] = [];
-    
-    if(root) {
-    queue.enqueue(root);
-        
-    }
-    
-    while(queue.length() > 0) {
-        const arr: number[] = [];
-        const length =  queue.length()
-        
-        for(let i = 0; i<length; i++) {
-            const cur = queue.dequeue();
-            arr.push(cur.val)
-            if(cur.left) {
-                queue.enqueue(cur.left)
-            }
-            if(cur.right) {
-                queue.enqueue(cur.right)
-            }
+    let q = [root], ans = []
+    while (q[0]) {
+        let qlen = q.length, row = []
+        for (let i = 0; i < qlen; i++) {
+            let curr = q.shift()
+            row.push(curr.val)
+            if (curr.left) q.push(curr.left)
+            if (curr.right) q.push(curr.right)
         }
-        result.push(arr)
+        ans.push(row)            
     }
-    return result
+    return ans
 };
 
