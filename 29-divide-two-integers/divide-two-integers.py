23f28fcbbacc7c2dc5e13cf3b65651ef4057de8f
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-01-25 10:56:24 +0700

    Time: 0 ms (100.00%) | Memory: 18 MB (15.28%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.py
+++ 29-divide-two-integers/divide-two-integers.py
@@ -1,5 +1,9 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2**31 and divisor == -1:
+            return 2**31 - 1
+        if dividend == -2**31 and divisor == 1:
+            return -2**31
         quotient = 0
         sign = 1
         if dividend < 0:
