9acf8c44a5a657e32a25b454f6b8df82dce49a9b
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-01-25 10:50:14 +0700

    Time: 4 ms (7.29%) | Memory: 17.7 MB (46.38%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.py
@@ -0,0 +1,28 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        quotient = 0
+        sign = 1
+        if dividend < 0:
+            sign *= -1
+        if divisor < 0:
+            sign *= -1  
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+        multiply = 1
+        tempDivisor = divisor
+
+
+        while tempDivisor<<1 < dividend:
+            multiply<<=1
+            tempDivisor<<=1
+        while dividend >= divisor:
+            if dividend >= tempDivisor:
+                dividend -= tempDivisor
+                quotient += multiply
+                
+            else:
+                multiply>>=1
+                tempDivisor>>=1
+        
+        return min(max(quotient * sign, -2 ** 31), 2 ** 31 -1)
+        
