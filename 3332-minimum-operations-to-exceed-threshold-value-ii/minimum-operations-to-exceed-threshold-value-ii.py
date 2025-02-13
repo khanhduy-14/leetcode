0d74dee9c1dcdd53b63a14ada6308e8592cb8cfb
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-02-14 00:08:47 +0700

    Time: 232 ms (58.82%) | Memory: 35.6 MB (25.21%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
@@ -1,64 +1,14 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        
-        heap = Heap(nums)
+        heapq.heapify(nums)
         res = 0
-        while heap.heap[1] < k:
+        x = heapq.heappop(nums)
+        while x < k:
             res += 1
-            minimum1 = heap.pop()
-            minimum2 = heap.pop()
-            newNum = min(minimum1, minimum2) * 2 + max(minimum1, minimum2)
-            heap.push(newNum)
+            y = heapq.heappop(nums)
+            newNum = min(x, y) * 2 + max(x, y)
+            heapq.heappush(nums, newNum)
+            x = heapq.heappop(nums)
         return res
-        
-
-class Heap:
-    def __init__(self, array: List[int]):
-        self.heapify(array)
-    def push(self, num: int):
-        self.heap.append(num)
-        i = len(self.heap) - 1
-
-        while i > 1 and self.heap[i] < self.heap[i // 2]:
-            self.heap[i], self.heap[i//2] = self.heap[i//2], self.heap[i]
-            i = i//2
-    def pop(self):
-        if len(self.heap) == 1:
-            return None
-        if len(self.heap) == 2:
-            return self.heap.pop()
-
-        res = self.heap[1]
-
-        self.heap[1] = self.heap.pop()
-
-        i = 1
-        while i * 2 < len(self.heap):
-            if i * 2 + 1 < len(self.heap) and self.heap[i*2+1] < self.heap[i*2] and self.heap[i*2+1] < self.heap[i]:
-                self.heap[i], self.heap[i*2 +1] = self.heap[i*2 + 1], self.heap[i]
-                i = i * 2 + 1
-            elif self.heap[i*2] < self.heap[i]:
-                self.heap[i], self.heap[i*2] = self.heap[i*2], self.heap[i]
-                i = i * 2
-            else:
-                break;
-        return res
-    def heapify(self, array: List[int]) -> List[int]:
-        array.append(array[0])
-        self.heap = array
-        curr = (len(array) - 1) // 2
-
-        while curr > 0:
-            i = curr
-            while i * 2 < len(self.heap):
-                if i * 2 + 1 < len(self.heap) and self.heap[i*2+1] < self.heap[i*2] and self.heap[i*2+1] < self.heap[i]:
-                    self.heap[i], self.heap[i*2 +1] = self.heap[i*2 + 1], self.heap[i]
-                    i = i * 2 + 1
-                elif self.heap[i*2] < self.heap[i]:
-                    self.heap[i], self.heap[i*2] = self.heap[i*2], self.heap[i]
-                    i = i * 2
-                else:
-                    break;
-            curr-=1
         
 
