87a3a9b9ba8d80f827fe719b4a612139b11bdcad
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-02-16 11:30:45 +0700

    Time: 184 ms (0.00%) | Memory: 42.4 MB (100.00%) - LeetSync

---
+++ 3779-eat-pizzas/eat-pizzas.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxWeight(self, pizzas: List[int]) -> int:
+        days = len(pizzas) // 4
+        pizzas.sort()
+        max_sum = 0
+        oddDays = (days + 1) // 2
+        evenDays = days // 2
+        print(oddDays, evenDays)
+        for i in range(oddDays):
+            max_sum+=pizzas.pop()
+        for i in range(evenDays):
+            pizzas.pop()
+            max_sum+=pizzas.pop()
+            
+        return max_sum
+            
