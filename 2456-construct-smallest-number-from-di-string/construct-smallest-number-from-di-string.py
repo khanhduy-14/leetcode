1ff32cd8a32e38824c3e0514f32f0e1b260af048
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-02-18 14:00:59 +0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (54.04%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -1,22 +1,14 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        valid_nums = []
-        used_nums= set()
-        def backtracking(string: str, min: int, max: int) -> str:
-            if len(string) == len(pattern) + 1:
-                valid_nums.append(int(string))
-            for i in range(1, len(pattern)+2):
-                if i not in used_nums and i > min and i < max:
-                    used_nums.add(i)
-                    if len(string) == len(pattern):
-                        valid_nums.append(int(string + str(i)))
-                    else:
-                        backtracking(string+str(i), 0 if pattern[len(string)] == 'D' else i,i if  pattern[len(string)] == 'D' else len(pattern)+2)
-                    used_nums.remove(i)
-                else:
-                    continue;
-        backtracking('', 0, len(pattern) + 2)
-        return str(min(valid_nums))
+        stack = []
+        res = ""
+        for i in range(1, len(pattern) + 2):
+            stack.append(i)
+
+            while stack and (i == len(pattern) + 1 or pattern[i-1] == 'I'):
+                res+=str(stack.pop())
+                
+        return res
                 
 
             
