aa3612f0934a1caee0bd74e337a2642fc57079b5
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-02-19 11:36:41 +0700

    Time: 3 ms (82.28%) | Memory: 18 MB (56.02%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,19 +1,21 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
         valid_chars = 'abc'
-        happy_strings = []
-        def backtracking(string):
-            if len(string) == n:
-                happy_strings.append(string)
-                return 
-            for i in range(len(valid_chars)):
-                prev_char = string[-1] if len(string) > 0 else None
-                if prev_char and prev_char == valid_chars[i]:
-                    continue
-                else:
-                    backtracking(string+valid_chars[i])
-        backtracking('')
-        return happy_strings[k-1] if 0 <= k-1 < len(happy_strings) else ''
+        total_happy_strings = 3 * pow(2, n-1)
+        if k > total_happy_strings:
+            return ''
+        res,left,right = '', 1, total_happy_strings
+        for i in range(n):
+            partition_size = (right - left + 1) / len(valid_chars)
+            selected_char_index = int((k - 1) // partition_size)
+            selected_char = valid_chars[selected_char_index]
+            res+=selected_char
+            valid_chars= 'abc'.replace(selected_char, '')
+            k = int(k % partition_size) if k > partition_size else k
+            left = (selected_char_index * partition_size) + 1
+            right = (selected_char_index + 1) * partition_size
+
+        return res
 
 
         
