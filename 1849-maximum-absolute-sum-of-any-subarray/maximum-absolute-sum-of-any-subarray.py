86ee8c87491129a35aabdb5c5d18f11cb2bd5a97
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-02-26 22:59:31 +0700

    Time: 100 ms (25.12%) | Memory: 28.5 MB (63.90%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
@@ -6,9 +6,8 @@ class Solution:
         for i in range(1, len(nums)):
             max_ending = max(max_ending + nums[i], nums[i])
             min_ending = min(min_ending + nums[i], nums[i])
-            abs_max_ending = max(abs(max_ending), abs(min_ending))
 
-            res = max(res, abs_max_ending)
+            res = max(res, abs(max_ending), abs(min_ending))
 
         return res
         
