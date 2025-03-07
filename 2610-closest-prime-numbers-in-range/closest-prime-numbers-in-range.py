56a8ccd819795b5dd682a86bd21a2c1379170fa1
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-03-07 21:41:09 +0700

    Time: 2502 ms (22.19%) | Memory: 35.2 MB (18.13%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
@@ -1,25 +1,21 @@
 class Solution:
     def closestPrimes(self, left: int, right: int) -> List[int]:
         prime = [True] * (right + 1)
-        def sieveOfEratosthenes():
-            for num in range(2, right+1):
-                if prime[num] == True and num * num <= right:
-                    for i in range(num * num, right+1, num):
-                        prime[i] = False
-        sieveOfEratosthenes() 
+        prime[0] = prime[1] = False
+        for num in range(2, right+1):
+            if prime[num] == True and num * num <= right:
+                for i in range(num * num, right+1, num):
+                    prime[i] = False
         res = [-1,-1]
         selected_primes = []
-        for i in range(max(left,2), right+1):
+        
+        for i in range(left, right+1):
             if prime[i]:
                 selected_primes.append(i)
+        if len(selected_primes) < 2:
+            return res
         for i in range(1, len(selected_primes)):
-            if res[0] == res[1] == -1:
-                res[1] = selected_primes[i]
-                res[0] = selected_primes[i-1]
-                continue
-            if selected_primes[i] - selected_primes[i-1] < res[1] - res[0]:
-                res[1] = selected_primes[i]
-                res[0] = selected_primes[i-1]
-        print(selected_primes)
+            if selected_primes[i] - selected_primes[i-1] < res[1] - res[0] or res[0] == -1:
+                res = [selected_primes[i-1],selected_primes[i]]
         return res
 
