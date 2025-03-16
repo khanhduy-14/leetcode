392b48a605a2231afacbf9926cde1a3081d3deb8
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-03-16 18:36:30 +0700

    Time: 443 ms (61.82%) | Memory: 22 MB (49.42%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -0,0 +1,34 @@
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        l, r, m_rank = 0, cars, min(ranks)
+
+
+
+        while l < r:
+            m = (l + r) // 2
+            count = 0
+            time = m*m * m_rank
+            for rank in ranks:
+                c_car = floor(sqrt(time/rank))
+                count+=c_car
+            if count >= cars:
+                r = m
+            else:
+                l = m + 1
+        l = m_rank * (r-1) * (r-1)
+        r = m_rank * r * r
+        while l < r:
+            count = 0
+            m = (l + r) // 2
+            for rank in ranks:
+                c_car = floor(sqrt(m/rank))
+                count+=c_car
+              
+            if count >= cars:
+                r = m
+            else:
+                l = m + 1
+        return l
+
+        
+        
