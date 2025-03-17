be49cf22b8badcff3ba6dee81d8fa2096bd89ffe
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-03-17 08:00:21 +0700

    Time: 4 ms (42.68%) | Memory: 17.8 MB (91.41%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
@@ -3,11 +3,11 @@ class Solution:
         s = set()
         n = len(nums)
         for i in range(n):
+            if len(s) > (n- i):
+                return False
             if nums[i] in s:
                 s.remove(nums[i])
             else:
                 s.add(nums[i])
-            if len(s) > (n- i -1):
-                return False
         return len(s) == 0
         
