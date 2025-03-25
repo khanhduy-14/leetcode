71f9868b8a7bf852d370195b571971144b125ac9
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-03-25 17:55:25 +0700

    Time: 11 ms (31.34%) | Memory: 21 MB (62.70%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -4,7 +4,6 @@ class Solution:
         res = [intervals[0]]
 
         for i in range(1,len(intervals)):
-            lastValue = res[len(res) - 1][1]
             if intervals[i][0] <= res[len(res) - 1][1]:
                res[len(res) - 1][1] = max(intervals[i][1], res[len(res) - 1][1])
             
