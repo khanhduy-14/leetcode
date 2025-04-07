b972ac3294d013418a1afffe4679350aa2792771
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-04-07 20:20:19 +0700

    Time: 35 ms (24.82%) | Memory: 18.4 MB (16.44%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,30 +1,40 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        
-        res,queen_positions =[], []
-        def check_valid_queen_position(r, c):
-            for [cr, cc] in queen_positions:
-                if cr == r or cc == c or abs(cr-r) == abs(cc-c):
-                    return False
-            
-            return True
-        if n == 1:
-            return [['Q']]
-        def backtrack(i, curr):
-            if i >= n:
-                res.append(curr.copy())
+        res = []
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
                 return
-            for j in range(n):
-                if check_valid_queen_position(i, j):
-                    row = ['.'] * n  
-                    row[j] = 'Q'    
-                    row = ''.join(row)
-                    queen_positions.append([i,j])
-                    curr.append(row)
-                    backtrack(i+1, curr)
-                    queen_positions.pop()
-                    curr.pop()
+            for c in range(n):
+                if self.isSafe(r, c, board):
+                    board[r][c] = "Q"
+                    backtrack(r + 1)
+                    board[r][c] = "."
 
-        backtrack(0, [])
-
+        backtrack(0)
         return res
+
+    def isSafe(self, r: int, c: int, board):
+        row = r - 1
+        while row >= 0:
+            if board[row][c] == "Q":
+                return False
+            row -= 1
+            
+        row, col = r - 1, c - 1
+        while row >= 0 and col >= 0:
+            if board[row][col] == "Q":
+                return False
+            row -= 1
+            col -= 1
+
+        row, col = r - 1, c + 1
+        while row >= 0 and col < len(board):
+            if board[row][col] == "Q":
+                return False
+            row -= 1
+            col += 1
+        return True
