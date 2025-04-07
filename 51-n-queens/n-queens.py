c37c525b513a7006547b7aa848de2f314a0ae3bf
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-04-07 18:28:00 +0700

    Time: 27 ms (33.07%) | Memory: 18.1 MB (96.00%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,29 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        
+        res,queen_positions =[], []
+        def check_valid_queen_position(r, c):
+            for [cr, cc] in queen_positions:
+                if cr == r or cc == c or abs(cr-r) == abs(cc-c):
+                    return False
+            
+            return True
+        if n == 1:
+            return [['Q']]
+        def backtrack(i, curr):
+            if i >= n:
+                res.append(curr.copy())
+                return
+            for j in range(n):
+                if check_valid_queen_position(i, j):
+                    row = '.' * n
+                    row = row[:j] + 'Q' + row[j+1:]
+                    queen_positions.append([i,j])
+                    curr.append(row)
+                    backtrack(i+1, curr)
+                    queen_positions.pop()
+                    curr.pop()
+
+        backtrack(0, [])
+
+        return res
