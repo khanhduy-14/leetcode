70fa864165c0ef36f52eadc14bb9694201059a94
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-04-07 20:52:57 +0700

    Time: 3 ms (7.92%) | Memory: 17.9 MB (34.19%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,13 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-         prev_rob = 0
-         profit = 0
-         for num in nums:
-            curr_rob = prev_rob + num
-            prev_rob=profit
-            profit = max(profit, curr_rob)
+        prevs, maxs = 0, 0
+
+        for i in range(len(nums)):
+            if prevs + nums[i] > maxs:
+                temp = prevs + nums[i]
+                prevs = maxs
+                maxs = temp
+            else:
+                prevs= maxs
+                
+        return maxs
+
+
+
+
         
-         return profit
-
-
         
