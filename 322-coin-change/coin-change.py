527a12622d6fc0c12153c74b68c987414011da8f
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-04-08 21:56:20 +0700

    Time: 707 ms (80.81%) | Memory: 18.3 MB (43.69%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,8 +1,6 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
-        if amount == 0:
-            return 0
         dp = [2**32 - 1] * (amount + 1)
         dp[0] = 0
         for coin in coins:
