8c6283ffd6bf93c1a8288c911fa867a6c3527924
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-04-12 23:09:32 +0700

    Time: 877 ms (33.84%) | Memory: 17.8 MB (96.73%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,19 +1,17 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         s = sum(nums)
-        # we can partition the array into two subsets with equal sum
-        # only when the sum is even
-        if s % 2: return False
-        # k is our target sum
-        k = s // 2
-        # subset sum problem -> solve it with DP
-        dp = [True] + [False] * k
-        # for each number
-        for x in nums:
-            # check all number from k to x
-            # to check if adding x can achieve i
-            for i in range(k, x - 1, -1): dp[i] |= dp[i - x]
-            # if dp[k] is achievable at a point, 
-            # then we can return True
-            if dp[k]: return True
-        return dp[k]
+        if s % 2 != 0:
+            return False
+        
+
+        n = len(nums)
+        target = s // 2
+        dp = [True] + [False] * s
+        for i in range(n):
+           
+
+            for j in range(s, -1, -1):
+                if dp[j]:
+                    dp[j + nums[i]] = True
+        return dp[target]
