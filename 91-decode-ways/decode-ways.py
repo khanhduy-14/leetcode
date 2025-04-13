769250c29cdd236b274172de115e26eb08a9e40b
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-04-13 12:36:08 +0700

    Time: 3 ms (27.06%) | Memory: 18.1 MB (9.56%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,17 +1,25 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0] == '0':
-            return 0
-        
-        n = len(s)
-        dp = [0] * (n + 1)
-        dp[0], dp[1] = 1, 1  
+        res = 0
+        can_group = 1
 
-        for i in range(1, n):
-            if s[i] != '0':  
-                dp[i + 1] = dp[i] 
-
-            two_digit = int(s[i - 1 : i + 1])
-            if 10 <= two_digit <= 26:  
-                dp[i + 1] += dp[i - 1]  
-        return dp[n]
+        for i in range(len(s)):
+            char = s[i]
+            prev_char = s[i-1] if i > 0 else ''
+            prev_res = res
+            
+            if int(char) >= 1 and int(char) <=26:
+                if int(prev_char+char) >= 1 and int(prev_char+char) <=26:
+                    res+=can_group
+                    can_group = prev_res if i > 0 else 1
+                else: 
+                    can_group = res
+            else:
+                if int(prev_char+char) >= 1 and int(prev_char+char) <=26:
+                    res=can_group if i > 0 else 0
+                else:
+                    res = 0
+                can_group = 0
+            print(res, can_group)
+          
+        return res
