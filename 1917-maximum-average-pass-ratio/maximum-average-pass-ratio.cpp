f35a55619661f61af167bfb694708b22d32c95f4
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-09-01 09:58:16 +0700

    Time: 400 ms (23.68%) | Memory: 110 MB (12.57%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -5,7 +5,9 @@ public:
         function<double(int, int)> gain = [](int p, int t) {
             return (double)(p + 1) / (t + 1) - (double)p / t;
         };
+        double ans  = 0.0;
         for (vector<int> c : classes) {
+            ans += (double) c[0] / c[1];
             q.push({gain(c[0], c[1]),c[0], c[1]});
         }
 
@@ -13,15 +15,11 @@ public:
             auto [gr, p, t] = q.top();
             q.pop();
             p++;t++;
+            ans += gr;
             q.push({gain(p,t), p, t});
         }
-        double ans  = 0.0;
-        while (!q.empty()) {
-            auto [gr, p, t] = q.top();
-            q.pop();
-            ans+= (double) p / t;
-            
-        }
+       
+    
         return ans / classes.size();
     }
 };
