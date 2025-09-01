f25f3f79b71dac93f0d64dc7743456485bf796b6
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-09-01 18:14:29 +0700

    Time: 0 ms (100.00%) | Memory: 9 MB (59.84%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,26 +1,18 @@
-vector<int> permutation(int n) {
-    string s = to_string(n);
-    sort(s.begin(), s.end()); 
-    vector<int> result;
-    
-    do {
-        if (s[0] != '0') {
-            result.push_back(stoi(s));
-        }
-    } while (next_permutation(s.begin(), s.end()));
-    
-    return result;
-}
+
 
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        vector<int> perms = permutation(n);
-        for (int p : perms) {
-            cout << p << endl;
-            if ((p & (p-1)) ==0) {
+        string s = to_string(n);
+        sort(s.begin(), s.end());
+       
+        for (int i = 0; i < 30; i++) {
+            int p2 = 1 << i;
+            string p2str = to_string(p2);
+            sort(p2str.begin(), p2str.end());
+            if(p2str== s) {
                 return true;
-            } 
+            }
         }
         return false;
     }
