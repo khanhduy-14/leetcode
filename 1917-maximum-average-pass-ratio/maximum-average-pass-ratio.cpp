c3a72f7a82cd6e27f93326564679cc81c384aae1
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-09-01 10:00:13 +0700

    Time: 395 ms (23.98%) | Memory: 109.8 MB (12.57%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -1,33 +1,26 @@
-using info = tuple<double, int, int>;
-info A[100000];
 class Solution {
 public:
-    static double maxAverageRatio(vector<vector<int>>& classes, int k) {
-        const int n = classes.size();
-        double sum = 0.0;
-        int i = 0;
-        for (auto& pq : classes) {
-            int p = pq[0], q = pq[1];
-            sum += (double)p/q;
-            double inc=(double)(q - p) / (q * (q + 1.0));
-            A[i++]={inc, p, q};
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        priority_queue<tuple<double,int,int>> q;
+        function<double(int, int)> gain = [](int p, int t) {
+            return (double)(p + 1) / (t + 1) - (double)p / t;
+        };
+        double ans  = 0.0;
+        for (vector<int> c : classes) {
+            ans += (double) c[0] / c[1];
+            q.push({gain(c[0], c[1]),c[0], c[1]});
         }
-        
-        make_heap(A, A+n);
-        
-        for (int i = 0; i < k; i++) {
-            pop_heap(A, A+n);
-            auto [r, p, q] = A[n-1];
-            if (r==0) break;// early stop
-            
-            // Add the current inc to the sum
-            sum += r;
-            double r2= (double)(q - p) / ((q +1.0)* (q + 2.0));
-            A[n-1]={ r2, p+1, q+1};
-            push_heap(A, A+n);
+
+        while(extraStudents--) {
+            auto [gr, p, t] = q.top();
+            q.pop();
+            p++;t++;
+            ans += gr;
+            q.push({gain(p,t), p, t});
         }
-        
-        return sum / n;
+       
+    
+        return ans / classes.size();
     }
 };
 
