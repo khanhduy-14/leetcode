d5a8efe79400ab9d6d7feb15c6212a4b90b5cfac
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-09-01 10:01:59 +0700

    Time: 387 ms (26.02%) | Memory: 98 MB (47.37%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-        priority_queue<tuple<double,int,int>> q;
-        function<double(int, int)> gain = [](int p, int t) {
+        auto gain = [](int p, int t) {
             return (double)(p + 1) / (t + 1) - (double)p / t;
         };
-        double ans  = 0.0;
-        for (vector<int> c : classes) {
-            ans += (double) c[0] / c[1];
-            q.push({gain(c[0], c[1]),c[0], c[1]});
+        
+        struct Node {
+            double g;
+            int p, t;
+        };
+        
+        struct Compare {
+            bool operator()(const Node& a, const Node& b) {
+                return a.g < b.g; // max-heap
+            }
+        };
+        
+        priority_queue<Node, vector<Node>, Compare> pq;
+        
+        for (auto& c : classes) {
+            pq.push({gain(c[0], c[1]), c[0], c[1]});
         }
-
-        while(extraStudents--) {
-            auto [gr, p, t] = q.top();
-            q.pop();
-            p++;t++;
-            ans += gr;
-            q.push({gain(p,t), p, t});
+        
+        while (extraStudents--) {
+            auto cur = pq.top(); pq.pop();
+            int p = cur.p + 1, t = cur.t + 1;
+            pq.push({gain(p, t), p, t});
         }
-       
-    
+        
+        double ans = 0.0;
+        while (!pq.empty()) {
+            auto cur = pq.top(); pq.pop();
+            ans += (double)cur.p / cur.t;
+        }
+        
         return ans / classes.size();
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
