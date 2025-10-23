787067c79f96d00c6b43ff17a7fe64ff73c413ef
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-10-24 00:26:12 +0700

    Time: 7 ms (57.91%) | Memory: 9.9 MB (62.96%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -5,8 +5,7 @@ public:
         while (s.length() > 2) {
             string temp = "";
             for (int i = 0; i < s.length() - 1; i++) {
-                int c1 = (int) s[i], c2 = (int) s[i+1];
-                temp+= ((c1+c2) % 10);
+                temp+= ((s[i] - '0' +  s[i+1] - '0') % 10);
             }
             s = temp;
         }
