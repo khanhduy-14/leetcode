75a6c1705b3847b177771ffc9f69e012aa4a0902
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-11-01 00:12:34 +0700

    Time: 61 ms (9.16%) | Memory: 25.3 MB (86.35%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        set<int> s;
-        vector<int> res;
-        for (int num: nums) {
-            if (s.count(num)) {
-                res.push_back(num);
+        int XOR = 0;
+        int i = 0;
+        while (i < nums.size()) {
+            XOR ^= nums[i];
+            if (i < (nums.size() - 2)) {
+                XOR ^= i;
             }
-            if (res.size() == 2) {
-                break;
-            }
-            s.insert(num);
+            i++;
         }
-        return res;
+        int right_most = XOR & -XOR, a = 0, b=0;
+        i=0;
+        while (i < nums.size()) {
+            if((nums[i] & right_most) == 0) {
+                a^=nums[i];
+            } else {
+                b^=nums[i];
+            }
+            if (i < (nums.size() - 2)) {
+                if((i & right_most) == 0) {
+                    a^=i;
+                } else {
+                    b^=i;
+                }
+            }
+            cout <<" " << a << " " << b;
+            i++;
+        }
+        return {a,b};
     }
 };
