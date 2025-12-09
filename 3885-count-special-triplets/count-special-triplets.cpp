7a4cb8d6ecf502ea1cbcd43e13ddd7ed8187a1fd
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-12-09 10:23:39 +0700

    Time: 51 ms (98.54%) | Memory: 198.9 MB (97.81%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -2,17 +2,21 @@ const int MOD = 1e9 + 7;
 class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
-        unordered_map<int, long long> prevC = {}, totalC = {};
-
-        for (int num:nums) {
-            totalC[num] +=1;
-        }
+        long long prevPairs[100001] = {0}, prevFreq[100001] = {0}, limit = 50000;
         long long res = 0;
-        for (int num:nums) {
-            res+= (prevC[num*2] * (totalC[num*2] - prevC[num*2] + (num == 0 ? -1 : 0)));
-            prevC[num]+=1;
-        }
 
-        return res % MOD;
+        for (int num: nums) {
+            
+            if(num%2==0) {
+                res = (res + prevPairs[num/2]);
+            }
+
+            if (num <= limit) {
+                prevPairs[num]+=prevFreq[num*2];
+            } 
+
+            prevFreq[num]+=1;
+        }
+        return res % MOD; 
     }
 };
