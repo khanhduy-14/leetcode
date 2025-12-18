d29909d8e9eeec9713943b442728c7e5e16bc1c0
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-12-18 16:17:25 +0700

    Time: 2 ms (97.13%) | Memory: 328.3 MB (22.98%) - LeetSync

--- 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.cpp
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -2,25 +2,30 @@ class Solution {
 public:
     long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
         int n = prices.size();
+        long long base_profit = 0, window_base_profit = 0, window_profit_gain = 0;
         vector<long long> prefix_sum(n, 0), prefix_base_profit(n, 0);
         prefix_sum[0] = prices[0];
         prefix_base_profit[0] =  prices[0] * strategy[0];
-        for (int i = 1 ; i < n; i++) {
-            prefix_sum[i] = prices[i] + prefix_sum[i-1];
-            prefix_base_profit[i] =  (prices[i] * strategy[i]) + prefix_base_profit[i-1];
+        for (int i = 0 ; i < n; i++) {
+            base_profit += (prices[i] * strategy[i]);
+            if (i < k) {
+                window_base_profit  = base_profit;
+                if (i >= k/2) {
+                    window_profit_gain+= prices[i];
+                }
+            }
+         
+        }
+        long long gain_profit = max(1LL * 0, window_profit_gain - window_base_profit);
+        for (int left = 1; left <= (n-k); left++) {
+            window_base_profit-= (prices[left-1] * strategy[left-1]);
+            window_base_profit+= (prices[left+k-1] * strategy[left+k-1]);
+            window_profit_gain-=prices[left+k/2 - 1];
+            window_profit_gain+=prices[left+k - 1];
+            gain_profit = max(gain_profit, window_profit_gain - window_base_profit);
         }
 
-        long long gain_profit = 0;
-
-        for (int left = 0; left <= (n-k); left++) {
-            int right = left + k - 1;
-            int mid = left + ((k-1) / 2);
-            int base_profit = prefix_base_profit[right] - (left > 0 ? prefix_base_profit[left-1] : 0);
-            gain_profit = max(gain_profit, (prefix_sum[right] - prefix_sum[mid])  - base_profit);
-
-        }
-
-        return prefix_base_profit[n-1]+ gain_profit;
+        return base_profit + gain_profit;
         
     }
 };
