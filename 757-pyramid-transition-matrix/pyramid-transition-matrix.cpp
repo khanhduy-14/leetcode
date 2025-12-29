94b0a7d28dcbddf50da00ed7a5a06e33efaae3aa
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-12-29 15:39:30 +0700

    Time: 1397 ms (24.33%) | Memory: 12 MB (88.65%) - LeetSync

---
+++ 757-pyramid-transition-matrix/pyramid-transition-matrix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    unordered_map<string, vector<char>> mp;
+    bool dfs(string& bottom) {
+        if (bottom.size() == 1)
+            return true;
+
+        string nextRow;
+        return buildNextRow(bottom, 0, nextRow);
+    }
+
+    bool buildNextRow(string &bottom, int index, string &nextRow) {
+        if (index == bottom.size() - 1) {
+            return dfs(nextRow);
+        }
+
+        string key  = bottom.substr(index, 2);
+
+        for (auto c : mp[key]) {
+            nextRow.push_back(c);
+            if(buildNextRow(bottom, index+1, nextRow)) {
+                return true;
+            }
+            nextRow.pop_back();
+        }
+
+        return false;
+    }
+    
+    bool pyramidTransition(string bottom, vector<string>& allowed) {
+   
+
+        for (string a : allowed) {
+            mp[a.substr(0, 2)].push_back(a[2]);
+        }
+
+
+        return dfs(bottom);
+    }
+};
