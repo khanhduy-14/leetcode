f70a1f7c61052d096cc24951b4133b0e644730fa
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2025-12-30 09:58:30 +0700

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.24%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.cpp
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -11,19 +11,13 @@ public:
         }
     
         for (int i = 0; i < 3; i++) {
-            int sum = 0;
-            for (int j = 0; j < 3; j++)
-                sum += grid[r + i][c + j];
-            if (sum != 15) return false;
+            if (grid[r + i][c] + grid[r + i][c + 1] + grid[r + i][c + 2] != 15)
+                return false;
+
+            if (grid[r][c + i] + grid[r + 1][c + i] + grid[r + 2][c + i] != 15)
+                return false;
         }
 
- 
-        for (int j = 0; j < 3; j++) {
-            int sum = 0;
-            for (int i = 0; i < 3; i++)
-                sum += grid[r + i][c + j];
-            if (sum != 15) return false;
-        }
 
         if (grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2] != 15) return false;
         if (grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != 15) return false;
