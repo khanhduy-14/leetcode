2bf1fd9ba632ad0543b90e0ff57fe32d571b49da
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2026-02-22 00:37:44 +0700

    Time: 3 ms (95.36%) | Memory: 20.2 MB (72.16%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -3,7 +3,6 @@ class Solution:
         ans = [0] * (n+1)
 
         for i in range(1, n+1):
-            prev = pow(2, math.floor(math.log2(i)))
-            ans[i] = 1 + ans[i - prev]
+            ans[i] = (i&1) + ans[i >> 1]
         
         return ans
