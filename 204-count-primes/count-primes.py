b58f690d174e85788312c5edafc0ae6dbcea6e30
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2026-02-21 21:58:21 +0700

    Time: 2463 ms (20.27%) | Memory: 58 MB (58.32%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -7,10 +7,9 @@ class Solution:
         for i in range(2, n):
             if x[i] == True:
                 res+=1
-                j = 2
-                print(i)
-                while i * j < n:
-                    x[i*j] = False
-                    j+=1
+                j = i * i
+                while j < n:
+                    x[j] = False
+                    j+=i
         
         return res
