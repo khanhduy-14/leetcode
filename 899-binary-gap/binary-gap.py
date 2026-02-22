64424046505326600be0b9544a37a8e81f8b0b97
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2026-02-22 10:32:51 +0700

    Time: 2 ms (12.15%) | Memory: 19.5 MB (9.94%) - LeetSync

--- 899-binary-gap/binary-gap.py
+++ 899-binary-gap/binary-gap.py
@@ -1,16 +1,15 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
+        if (n & (n - 1)) == 0: return 0
+        n //= (n & -n)
         res = 0
         curr = 0
         while n:
             if n & 1:
                 res = max(res, curr)
-                curr = 0
-                curr+=1
-
+                curr = 1
             else:
-                if curr:
-                    curr+=1
+                curr+=1
             n = n >> 1
         return res
         
