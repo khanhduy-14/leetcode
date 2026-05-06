866870cf743d0c553dd5f2d7ac95cc8d465ab227
khanhduy-14 <101503338+khanhduy-14@users.noreply.github.com>
2026-05-07 00:50:32 +0700

    Time: 132 ms (52.94%) | Memory: 52.1 MB (85.19%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.py
+++ 1972-rotating-the-box/rotating-the-box.py
@@ -1,31 +1,20 @@
 class Solution:
     def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
-        rows = len(boxGrid)
-        cols = len(boxGrid[0])
-        rotated_grid = [['' for _ in range(rows)] for _ in range(cols)]
-
-
+        rows, cols = len(boxGrid), len(boxGrid[0])
 
         for i in range(rows):
-            stone = 0
-            for j in range(cols):
-                if boxGrid[i][j] == '#':
+            write = cols - 1
+            for j in range(cols - 1, -1, -1):
+                if boxGrid[i][j] == '*':
+                    write = j - 1
+                elif boxGrid[i][j] == '#':
                     boxGrid[i][j] = '.'
-                    stone+=1
+                    boxGrid[i][write] = '#'
+                    write -= 1
 
-                if (boxGrid[i][j] == '*' or j == cols - 1) and stone > 0:
-                    carry = 1 if j == cols - 1 and boxGrid[i][j] == '.' else 0
-                    while stone > 0:
-                        boxGrid[i][j-stone + carry] = '#'
-                        stone-=1
-
+        rotated = [[None] * rows for _ in range(cols)]
         for i in range(rows):
             for j in range(cols):
-                rotated_grid[j][rows - 1 - i] = boxGrid[i][j]
+                rotated[j][rows - 1 - i] = boxGrid[i][j]
 
-
-        return rotated_grid
-
-
-
-                
+        return rotated
